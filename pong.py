34bc172b5485aaae7a0852da8edb35c88ab06f46
Enrique Cabrini <enrique.cabrini@gmail.com>
2017-07-26 00:26:48 +0000

    added FPS. refs #5

--- pong.py
+++ pong.py
@@ -3,10 +3,14 @@
 import sys
 import pygame
 
+FPS = 60
+
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Pong")
+clock = pygame.time.Clock()
 while True:
+    dt = clock.tick(FPS) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)

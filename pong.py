86d8b25b229c6cb4c4d1388c3ab19fc8f5be7579
Enrique Cabrini <enrique.cabrini@gmail.com>
2017-07-30 00:08:36 +0000

    added Paddles refs #2

--- pong.py
+++ pong.py
@@ -14,12 +14,36 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, x):
+        self.group = all_sprites
+        pygame.sprite.Sprite.__init__(self, self.group)
+        self.x = x
+        self.y = HEIGHT / 2
+        self.image = pygame.Surface((20,100))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, HEIGHT / 2)
+ 
+def draw():
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(screen)
+        pygame.display.flip()
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+left_paddle = Paddle(30)
+right_paddle = Paddle(WIDTH - 30)
+all_sprites.add(left_paddle)
+all_sprites.add(right_paddle)
 while True:
     dt = clock.tick(FPS) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
